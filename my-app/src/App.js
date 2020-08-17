1c091eb379a3d67f84a9cebca79828ee8e189404
Gedalya Krycer <gedalya@krycer.com>
2020-08-17 13:17:46 -0700

    Added logo to navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from './img/digibits-logo.svg';
 
 
 function App() {
   return (
     <>
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>
+          <img src={Logo} alt="DigiBit Logo" />
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="#features">Features</Nav.Link>
+        </Nav>
+      </Navbar>
       <h1>Team Directory</h1>
       <h2>Jocelyn Curtis</h2>
       <h3>Designer</h3>
       <h4>Location</h4>
       <p>jcurtis@digidigits.com</p>
+
+
     </>
   );
 }
