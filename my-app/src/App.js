a5540dd0ea77e0021646610c172826f046bf8340
Gedalya Krycer <gedalya@krycer.com>
2020-08-17 14:14:26 -0700

    Added h1 to header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import Logo from './img/digibits-logo.svg';
 import { FaGithub } from 'react-icons/fa';
 
@@ -8,15 +9,24 @@ import { FaGithub } from 'react-icons/fa';
 function App() {
   return (
     <>
-      <Navbar expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>
-          <img src={Logo} alt="DigiBit Logo" />
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="https://github.com/GedalyaKrycer"><FaGithub /></Nav.Link>
-        </Nav>
-      </Navbar>
-      <h1>Team Directory</h1>
+      <header>
+        <Container>
+          <Navbar className="justify-content-between">
+            <Navbar.Brand>
+              <img src={Logo} alt="DigiBit Logo" />
+            </Navbar.Brand>
+            <Nav>
+              <Nav.Link href="https://github.com/GedalyaKrycer"><FaGithub /></Nav.Link>
+            </Nav>
+
+          </Navbar>
+          <h1 className="text-center">Team Directory</h1>
+        </Container>
+      </header>
+
+
+
+
       <h2>Jocelyn Curtis</h2>
       <h3>Designer</h3>
       <h4>Location</h4>
