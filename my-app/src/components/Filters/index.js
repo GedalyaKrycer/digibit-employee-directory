f4430f64e7b0655350f5ebda0d5c02742527f339
Gedalya Krycer <gedalya@krycer.com>
2020-08-18 17:46:38 -0700

    Added css for filtering bar

--- my-app/src/components/Filters/index.js
+++ my-app/src/components/Filters/index.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import './style.css';
-import { ButtonGroup, Button, DropdownButton, Dropdown } from 'react-bootstrap'
+import { FaSearch } from 'react-icons/fa';
+import {
+    ButtonGroup,
+    Button,
+    DropdownButton,
+    Dropdown,
+    Form,
+    Row,
+    Col
+} from 'react-bootstrap'
 
 
 function Filters() {
     return (
-        <section className="row">
-            <div className="col-md-4">
-                <ButtonGroup>
-                    <Button>A-Z Sort</Button>
+        <Row>
+            <Col md={4}>
+                <Form className="has-search margin">
+                    <FaSearch className="form-control-feedback search-icon" />
+                    <Form.Control type="search" placeholder="Search" className="search-input" />
+                </Form>
+            </Col>
+            <Col md={8} className="button-group">
+                <ButtonGroup className="margin full-width">
+                    <Button className="third-width">A-Z Sort</Button>
 
-                    <DropdownButton as={ButtonGroup} title="Title" id="bg-nested-dropdown">
+                    <DropdownButton as={ButtonGroup} title="Title" id="bg-nested-dropdown" className="third-width">
                         <Dropdown.Item eventKey="1">Manager</Dropdown.Item>
                         <Dropdown.Item eventKey="2">Designer</Dropdown.Item>
                         <Dropdown.Item eventKey="3">Developer</Dropdown.Item>
                     </DropdownButton>
-                    <DropdownButton as={ButtonGroup} title="Location" id="bg-nested-dropdown" className="border-radius">
+                    <DropdownButton as={ButtonGroup} title="Location" id="bg-nested-dropdown" className="border-radius third-width">
                         <Dropdown.Item eventKey="1">New York</Dropdown.Item>
                         <Dropdown.Item eventKey="2">Las Vegas</Dropdown.Item>
                         <Dropdown.Item eventKey="3">Chicago</Dropdown.Item>
                     </DropdownButton>
                 </ButtonGroup>
-            </div>
-            <div className="col-md-6">
-                
-            </div>
+            </Col>
 
-        </section>
+        </Row>
 
     )
 }
