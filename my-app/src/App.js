acb8a1a294cd893b762b6b1853f817a0a33e9abb
Gedalya Krycer <gedalya@krycer.com>
2020-08-18 17:58:32 -0700

    Created TeamCard component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Filters from "./components/Filters";
+import TeamCard from "./components/TeamCard";
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <Header />
       <Main>
         <Filters />
+        <TeamCard />
+
         <h2>Jocelyn Curtis</h2>
         <h3>Designer</h3>
         <h4>Location</h4>
---
+++ my-app/src/components/TeamCard/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './style.css';
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
+import {
+    ButtonGroup,
+    Button,
+    DropdownButton,
+    Dropdown,
+    Form,
+    Row,
+    Col
+} from 'react-bootstrap'
+
+
+function Filters() {
+    return (
+        <Row>
+            <Col md={4}>
+                <Form className="has-search margin">
+                    <FaSearch className="form-control-feedback search-icon" />
+                    <Form.Control type="search" placeholder="Search" className="search-input" />
+                </Form>
+            </Col>
+            <Col md={8} className="button-group">
+                <ButtonGroup className="margin full-width">
+                    <Button className="third-width">A-Z Sort</Button>
+
+                    <DropdownButton as={ButtonGroup} title="Title" id="bg-nested-dropdown" className="third-width">
+                        <Dropdown.Item eventKey="1">Manager</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Designer</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">Developer</Dropdown.Item>
+                    </DropdownButton>
+                    <DropdownButton as={ButtonGroup} title="Location" id="bg-nested-dropdown" className="border-radius third-width">
+                        <Dropdown.Item eventKey="1">New York</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Las Vegas</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">Chicago</Dropdown.Item>
+                    </DropdownButton>
+                </ButtonGroup>
+            </Col>
+
+        </Row>
+
+    )
+}
+
+
+export default Filters;
