b3675eaef9a6e313f7342f7495af3ac2f5dc0b2d
Gedalya Krycer <gedalya@krycer.com>
2020-08-18 22:16:06 -0700

    Fixed flex issues with filter button group

--- my-app/src/components/Filters/index.js
+++ my-app/src/components/Filters/index.js
@@ -23,20 +23,20 @@ function Filters() {
             </Col>
             <Col md={8} className="button-group">
                 <ButtonGroup className="margin full-width">
-                    <Button className="third-width">A-Z Sort</Button>
+                    <Button>A-Z Sort</Button>
 
-                    <DropdownButton as={ButtonGroup} title="Title" id="bg-nested-dropdown" className="third-width">
+                    <DropdownButton as={ButtonGroup} title="Title" id="bg-nested-dropdown">
                         <Dropdown.Item eventKey="1">Manager</Dropdown.Item>
                         <Dropdown.Item eventKey="2">Designer</Dropdown.Item>
                         <Dropdown.Item eventKey="3">Developer</Dropdown.Item>
                     </DropdownButton>
 
-                    <DropdownButton as={ButtonGroup} title="Location" id="bg-nested-dropdown" className="border-radius third-width">
+                    <DropdownButton as={ButtonGroup} title="Location" id="bg-nested-dropdown">
                         <Dropdown.Item eventKey="1">New York</Dropdown.Item>
                         <Dropdown.Item eventKey="2">Las Vegas</Dropdown.Item>
                         <Dropdown.Item eventKey="3">Chicago</Dropdown.Item>
                     </DropdownButton>
-                    <Button className="third-width">Reset</Button>
+                    <Button>Reset</Button>
                 </ButtonGroup>
             </Col>
 
