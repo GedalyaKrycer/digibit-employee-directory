032fdd69f867541a603f738985787a90eeccf3e7
Gedalya Krycer <gedalya@krycer.com>
2020-08-21 02:19:14 -0700

    Added react build files

---
+++ my-app/build/precache-manifest.c57285caed9abe4c8bebba89c070c19e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96c5c02dc74d388cf78750f953013cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1193484229fe6894e1b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b190caf02557fa9f99b9",
+    "url": "/static/css/main.3358152d.chunk.css"
+  },
+  {
+    "revision": "b1193484229fe6894e1b",
+    "url": "/static/js/2.36f27ca9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.36f27ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b190caf02557fa9f99b9",
+    "url": "/static/js/main.221845ad.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "85cd4589ac861d33fb6bcb02d6fdb4fc",
+    "url": "/static/media/background-hero.85cd4589.jpg"
+  },
+  {
+    "revision": "d64045a5d5c1ae543ea9b5371ffcafa6",
+    "url": "/static/media/digibits-logo.d64045a5.svg"
+  }
+]);
