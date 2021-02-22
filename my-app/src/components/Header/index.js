12b02de0a1124c7bab34d987667105137480071d
Yonathan Goriachnick <yonathan@200apps.co>
2021-02-22 15:52:14 +0200

    added klika title and logo

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import Logo from '../../img/digibits-logo.svg';
+import Logo from '../../img/Logo_Klika.svg';
 import { FaGithub } from 'react-icons/fa';
 import './style.css';
 
@@ -13,14 +13,14 @@ function Header() {
             <Container>
                 <Navbar className="justify-content-between">
                     <Navbar.Brand>
-                        <img src={Logo} alt="DigiBit Logo" />
+                        <img src={Logo} alt="Klika Logo" />
                     </Navbar.Brand>
                     <Nav>
-                        <Nav.Link href="https://github.com/GedalyaKrycer/unit-19-react-homework-employee-directory" target="_Blank"><FaGithub className="github-icon" /></Nav.Link>
+                        <Nav.Link href="https://github.com/yoni-g/digibit-employee-directory" target="_Blank"><FaGithub className="github-icon" /></Nav.Link>
                     </Nav>
 
                 </Navbar>
-                <h1 className="text-center text-color">Team Directory</h1>
+                <h1 className="text-center text-color">חברי קליקה</h1>
             </Container>
         </header>
     )
